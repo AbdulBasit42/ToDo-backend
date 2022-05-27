b76d4b3c80b2427a4552337ed613bd165e34feff
Abdul Basit <abdulbasit526492@gmail.com>
2022-05-27 23:19:21 +0530

    Host on heroku

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+var items=[];
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    var today = new Date();
+
+    var options = {
+        weekday: "long", 
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("lists", { kindOfDay: day, itemAdds: items })
+})
+
+app.post("/", (req, res) => {
+    var item = req.body.item;
+    items.push(item);
+    console.log(item);
+    res.redirect("/");
+
+})
+
+app.listen(3000, () => {
+
+    console.log("Server started at port 3000");
+})
